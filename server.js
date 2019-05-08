92d09a15dbff93a254b76500906c93ac385a227b
kprabhanew <kprabhanew@gmail.com>
2019-05-09 01:00:04 +0530

    Render html page

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ var http = require('http');
 //Configure our HTTP server to respond with Hello to all requests.
 var server = http.createServer(function(requests, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
+    /*
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write('<!DOCTYPE html>');
+    response.write('<html>');
+    response.write('<head>');
+    response.write('<title>Hello World Page </title>');
+    response.write('</head>');
+    response.write('<body>');
+    response.write("Hello World !");
+    response.write('</body>');
+    response.write('</html>');
+    response.end();
+    */
     response.end('Hello World\n');
 });
 
